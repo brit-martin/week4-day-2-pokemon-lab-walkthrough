2267d2831c27110174cbe022d9c12ca8c28b3d2a
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2023-12-19 16:22:56 -0700

    adds form and associated things

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,35 @@ const theList = ['pikachu', 'raichu', 'voltorb']
 function App() {
 
   const [pokemonList, setPokemonList] = useState(theList)
+  const [limit, setLimit] = useState('2')
+  const [type, setType] = useState('electric')
+
 
 function handleClick(){
   setPokemonList([...pokemonList, 'Mewtwo'])
+}
+
+function handleLimitChange(event){
+setLimit(event.target.value)
+}
+
+
+function handleTypeChange(event){
+  setType(event.target.value)
 }
 
 
   return (
     <div>
      <h1>Here are some Pokemon!</h1>
+     <form>
+      <label htmlFor="limit-input">Limit: </label>
+      <input id="limit-input" value= {limit} onChange={handleLimitChange}/>
+      <label htmlFor="type-input">Type: </label>
+      <input id="type-input" value= {type} onChange={handleTypeChange}/>
+      <button>Create</button>
+    </form>
+
       {
         pokemonList.map((element, index, array) => {
           return <Card pokemon={element}/>
