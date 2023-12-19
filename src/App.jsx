35635e06478639bb9780a0486cff7d2820a9c210
Brittany Larson <brittanylarson@Brittanys-Air.localdomain>
2023-12-19 14:56:48 -0700

    makes alem child component(lame because it is static

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-
+import Card from './Card.jsx'
 import './App.css'
 
 function App() {
@@ -6,7 +6,10 @@ function App() {
 
   return (
     <>
-     <h1>hey</h1>
+     <h1>Here are some Pokemon!</h1>
+     <Card/>
+     <Card/>
+     <Card/>
     </>
   )
 }
